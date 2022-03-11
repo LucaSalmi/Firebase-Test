f3514197e13cd5847eac8d1c7933cef649a542ac
Luca Salmi <luca.salmi90@gmail.com>
2022-03-11 08:39:57 +0100

    Initial Commit

---
+++ Firebase Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Firebase Test
+//
+//  Created by Luca Salmi on 2022-03-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Firebase Test/Firebase_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_TestApp.swift
+//  Firebase Test
+//
+//  Created by Luca Salmi on 2022-03-11.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
